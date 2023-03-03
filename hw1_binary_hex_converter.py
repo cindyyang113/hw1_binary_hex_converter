54e28b98db71c3b23a3c9a47cec78cd338ba6c4e
cindyyang113 <126647221+cindyyang113@users.noreply.github.com>
2023-03-03 15:37:14 +0800

    Update hw1_binary_hex_converter.py

--- hw1_binary_hex_converter.py
+++ hw1_binary_hex_converter.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
+# 2進位轉換成16進位:
 
 #get input and initialize variables
 decimal = int(input("Enter a decimal number \n"))
@@ -20,11 +19,28 @@ while(temp > 0):
 print("Binary of {x} is: {y}".format(x=decimal,y=binary))
 
 
-# In[14]:
+# 10進位轉換成16進位:
 
-
+# Conversion table of remainders to
+# hexadecimal equivalent
+conversion_table = {0: '0', 1: '1', 2: '2', 3: '3', 4: '4',
+                    5: '5', 6: '6', 7: '7',
+                    8: '8', 9: '9', 10: 'A', 11: 'B', 12: 'C',
+                    13: 'D', 14: 'E', 15: 'F'}
+ 
+ 
+# function which converts decimal value
+# to hexadecimal value
 decimal = int(input("Enter a decimal number \n"))
-answer = hex(decimal)
-answer = (answer[2:])
-print("Hexadecimal of {x} is: {y}".format(x=decimal,y=answer))
-
+def decimalToHexadecimal(decimal):
+    hexadecimal = ''
+    while(decimal > 0):
+        remainder = decimal % 16
+        hexadecimal = conversion_table[remainder] + hexadecimal
+        decimal = decimal // 16
+ 
+    return hexadecimal
+ 
+ 
+print("The hexadecimal form of", decimal,
+      "is", decimalToHexadecimal(decimal))
